2b8826f67e4c12de44835a4c2990621dc0d348c1
Nuno Mata <nunomata619@gmail.com>
2021-06-06 23:25:38 +0100

    db updates

--- mongodb.js
+++ mongodb.js
@@ -9,63 +9,18 @@ const { MongoClient, ObjectID} = require('mongodb')
 const connectionURL ='mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error){
         return console.log('unable to connect to databse')
     }
 
     const db = client.db(databaseName)
 
-db.collection('users').insertOne({
-    _id: id,
-    name: 'Turtle',
-    age: 26
-}, (error, result) => {
-    if(error){
-        return console.log('Unablle to insert user')
-    }
-
-    console.log(result.ops)
-})
-
-// db.collection('users').insertMany([
-//     {
-//         name:'Billy',
-//         age: 23
-//     }, {
-//         name: 'Niggy',
-//         age: 53
-//     }
-// ], (error, result)=> {
-//     if(error){
-//         return console.log('Unable to insert')
-//     }
-
-//     console.log(result.ops)
-// })
-
-
-// db.collection('tasks').insertMany([
-//     {
-//         description:'buy doge',
-//         completed: true
-//     }, {
-//         description:'cook fish',
-//         completed: false
-//     }, {
-//         description:'caydino poopo',
-//         completed: false
-//     }
-// ], (error, result)=> {
-//     if(error){
-//         return console.log('Unable to insert tasks!')
-//     }
-
-//     console.log(result.ops)
-// })
-
-})
+    db.collection('tasks').deleteOne({ 
+        description : "buy doge"}).then ((result) =>{
+            console.log(result)
+        }).catch((error) =>{
+            console.log(error)
+        })
+     })
